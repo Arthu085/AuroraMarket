2454d2ca27badc538223301e70523d8996127593
Arthu085 <arthurghizi22@gmail.com>
2025-01-14 13:47:52 -0300

    HTML e CSS do header finalizado

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -9,15 +9,21 @@ const Header = () => {
                 <div className='link-home'>
                     <Link to='/' className='home-link'>AURORA MARKET</Link>
                 </div>
-                <div className='link-pages'>
-                    <Link to='' className='page-link'>Anéis</Link>
-                    <Link to='' className='page-link'>Alianças</Link>
-                    <Link to='' className='page-link'>Brincos</Link>
-                    <Link to='' className='page-link'>Colares</Link>
-                    <Link to='' className='page-link'>Correntes</Link>
-                    <Link to='' className='page-link'>Pingentes</Link>
-                    <Link to='' className='page-link'>Pulseiras</Link>
-                    <Link to='' className='page-link'>Ver Tudo</Link>
+                <div className='links-header'>
+                    <div className='link-pages'>
+                        <Link to='' className='page-link'>Anéis</Link>
+                        <Link to='' className='page-link'>Alianças</Link>
+                        <Link to='' className='page-link'>Brincos</Link>
+                        <Link to='' className='page-link'>Colares</Link>
+                        <Link to='' className='page-link'>Correntes</Link>
+                        <Link to='' className='page-link'>Pingentes</Link>
+                        <Link to='' className='page-link'>Pulseiras</Link>
+                        <Link to='' className='page-link'>Ver Tudo</Link>
+                    </div>
+                    <div className='login-carrinho'>
+                        <Link to='' className='page-link'>Entre ou <br/>cadastre-se</Link>
+                        <Link to='' className='page-link'><img src="carrinho-de-compras.png" alt="Carrinho de Compras" /></Link>
+                    </div>
                 </div>
             </nav>
         </header>
